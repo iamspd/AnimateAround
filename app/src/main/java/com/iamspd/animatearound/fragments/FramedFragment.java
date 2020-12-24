c967ff662a2a00444e9366178eb1676a96afb7c5
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2020-12-24 11:51:57 -0500

    fragment_earth_moon.xml Created

--- app/src/main/java/com/iamspd/animatearound/fragments/FramedFragment.java
+++ app/src/main/java/com/iamspd/animatearound/fragments/FramedFragment.java
@@ -24,12 +24,25 @@ public class FramedFragment extends Fragment {
     // drawable graphic
     private AnimationDrawable fAnimationDrawable = null;
 
+    /**
+     * method to provide a fragment view on the screen
+     * @param inflater inflates the layout to the fragment
+     * @param container the view where the layout is going to be inflated
+     * @param savedInstanceState state of the layout at which it is inflated
+     * @return view on the screen
+     */
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_framed, container, false);
     }
 
+    /**
+     * method to handle operations on view
+     * @param rootView is where the actual view is created
+     * @param savedInstanceState state of the view at a time
+     */
     @Override
     public void onViewCreated(@NonNull View rootView, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(rootView, savedInstanceState);
@@ -42,6 +55,9 @@ public class FramedFragment extends Fragment {
         fAnimStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // on a click of a start button, method is called
+                //  to start the animation
                 startFrameAnimation();
             }
         });
@@ -50,12 +66,20 @@ public class FramedFragment extends Fragment {
         fAnimStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // on a click of a button, a method is called
+                //  to stop the animation
                 stopFrameAnimation();
             }
         });
     }
 
+    /**
+     * method to handle animation of framed images
+     *  to animate them in given time duration
+     */
     public void startFrameAnimation() {
+
         BitmapDrawable img1 = (BitmapDrawable) getResources().getDrawable(R.drawable.img1);
         BitmapDrawable img2 = (BitmapDrawable) getResources().getDrawable(R.drawable.img2);
         BitmapDrawable img3 = (BitmapDrawable) getResources().getDrawable(R.drawable.img3);
@@ -82,6 +106,9 @@ public class FramedFragment extends Fragment {
         fAnimationDrawable.start();
     }
 
+    /**
+     * method to clear or stop the animation from the state
+     */
     public void stopFrameAnimation() {
         fAnimationDrawable.stop();
         fAnimationDrawable.setVisible(false, false);
